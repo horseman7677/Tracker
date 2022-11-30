5766acbf2f27acb4476083de2d194d77dbef68b3
horseman7677 <70053149+horseman7677@users.noreply.github.com>
2022-12-01 00:32:42 +0530

    sign in update

--- app/src/main/java/com/horseman/tracker/MainActivity.java
+++ app/src/main/java/com/horseman/tracker/MainActivity.java
@@ -2,19 +2,32 @@ package com.horseman.tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.TextView;
 
 import com.horseman.tracker.onBoarding.OnBoarding;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView signupTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        signupTxt = findViewById(R.id.signupTxtLogin);
+        signupTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignUp.class));
 
+            }
+        });
     }
 }
